5c6a5ad3a76d238ed1126c4ff912ba29ec658c07
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2022-11-22 14:04:12 +0100

    Added code

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Övningar
 
 Dela upp solaris och hänga gubbe i lämpliga moduler.
+
+**Veckans övning:** https://gist.github.com/zocom-christoffer-wallenberg/8f7b793fdf34e3d9404f7123c8aea4d4
--- modules-example/js/index.js
+++ modules-example/js/index.js
@@ -1,4 +1,4 @@
-import { getJokes } from './modules/api.js';
+import { getJokes } from './modules/api.js'; /** ./ betyder starta i nuvarande mapp */
 import { display } from './modules/display.js';
 
 async function start() {
